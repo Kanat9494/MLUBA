b5565e4294b6b665b7b7fee165b178109a83bcd0
kana2022 <kkanat94@gmail.com>
2023-05-15 17:54:04 +0600

    15052023

--- MLUBA/wwwroot/js/main.js
+++ MLUBA/wwwroot/js/main.js
@@ -1,3 +1,13 @@
 ﻿function testAlert() {
 	alert("Это тестовый алёрт!");
 }
+
+function clearNavigationStack() {
+    // Очистка истории навигации
+    history.pushState(null, '', location.href);
+    history.replaceState(null, '', location.href);
+}
+
+function displayConfirm(question) {
+    confirm(question);
+}
